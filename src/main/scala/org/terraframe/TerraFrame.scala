170eb55485edab7c8ed9502f603825be452b5f4a
Shawn Garner <shawndgarner@gmail.com>
2017-08-06 16:47:15 -0500

    moving out of init into vals

--- src/main/scala/org/terraframe/TerraFrame.scala
+++ src/main/scala/org/terraframe/TerraFrame.scala
@@ -1239,7 +1239,54 @@ object TerraFrame {
     maxStacksTemp.asScala.toMap
   }
   var SKYLIGHTS: Map[Int, Int] = _
-  var SKYCOLORS: Map[Int, Color] = _
+  val SKYCOLORS: Map[Int, Color] = {
+    val skyColorsTemp = new jul.HashMap[Int, Color](50)
+
+    skyColorsTemp.put(28800, new Color(71, 154, 230))
+    skyColorsTemp.put(28980, new Color(67, 146, 218))
+    skyColorsTemp.put(29160, new Color(63, 138, 206))
+    skyColorsTemp.put(29340, new Color(60, 130, 194))
+    skyColorsTemp.put(29520, new Color(56, 122, 182))
+    skyColorsTemp.put(29700, new Color(52, 114, 170))
+    skyColorsTemp.put(29880, new Color(48, 105, 157))
+    skyColorsTemp.put(30060, new Color(45, 97, 145))
+    skyColorsTemp.put(30240, new Color(41, 89, 133))
+    skyColorsTemp.put(30420, new Color(37, 81, 121))
+    skyColorsTemp.put(30600, new Color(33, 73, 109))
+    skyColorsTemp.put(30780, new Color(30, 65, 97))
+    skyColorsTemp.put(30960, new Color(26, 57, 85))
+    skyColorsTemp.put(31140, new Color(22, 48, 72))
+    skyColorsTemp.put(31320, new Color(18, 40, 60))
+    skyColorsTemp.put(31500, new Color(15, 32, 48))
+    skyColorsTemp.put(31680, new Color(11, 24, 36))
+    skyColorsTemp.put(31860, new Color(7, 16, 24))
+    skyColorsTemp.put(32040, new Color(3, 8, 12))
+    skyColorsTemp.put(32220, new Color(0, 0, 0))
+
+    skyColorsTemp.put(72000, new Color(3, 8, 12))
+    skyColorsTemp.put(72180, new Color(7, 16, 24))
+    skyColorsTemp.put(72360, new Color(11, 24, 36))
+    skyColorsTemp.put(72540, new Color(15, 32, 48))
+    skyColorsTemp.put(72720, new Color(18, 40, 60))
+    skyColorsTemp.put(72900, new Color(22, 48, 72))
+    skyColorsTemp.put(73080, new Color(26, 57, 85))
+    skyColorsTemp.put(73260, new Color(30, 65, 97))
+    skyColorsTemp.put(73440, new Color(33, 73, 109))
+    skyColorsTemp.put(73620, new Color(37, 81, 121))
+    skyColorsTemp.put(73800, new Color(41, 89, 133))
+    skyColorsTemp.put(73980, new Color(45, 97, 145))
+    skyColorsTemp.put(74160, new Color(48, 105, 157))
+    skyColorsTemp.put(74340, new Color(52, 114, 170))
+    skyColorsTemp.put(74520, new Color(56, 122, 182))
+    skyColorsTemp.put(74700, new Color(60, 130, 194))
+    skyColorsTemp.put(74880, new Color(63, 138, 206))
+    skyColorsTemp.put(75060, new Color(67, 146, 218))
+    skyColorsTemp.put(75240, new Color(71, 154, 230))
+    skyColorsTemp.put(75420, new Color(75, 163, 243))
+
+    skyColorsTemp.asScala.toMap
+
+  }
   var LIGHTLEVELS: Map[Int, BufferedImage] = _
   val blockImgs: Map[String, BufferedImage] = {
     val blockImgsTemp = new jul.HashMap[String, BufferedImage](blocknames.length)
@@ -1502,51 +1549,6 @@ class TerraFrame extends JApplet
 
       repaint()
 
-      val skyColorsTemp = new jul.HashMap[Int, Color](50)
-
-      skyColorsTemp.put(28800, new Color(71, 154, 230))
-      skyColorsTemp.put(28980, new Color(67, 146, 218))
-      skyColorsTemp.put(29160, new Color(63, 138, 206))
-      skyColorsTemp.put(29340, new Color(60, 130, 194))
-      skyColorsTemp.put(29520, new Color(56, 122, 182))
-      skyColorsTemp.put(29700, new Color(52, 114, 170))
-      skyColorsTemp.put(29880, new Color(48, 105, 157))
-      skyColorsTemp.put(30060, new Color(45, 97, 145))
-      skyColorsTemp.put(30240, new Color(41, 89, 133))
-      skyColorsTemp.put(30420, new Color(37, 81, 121))
-      skyColorsTemp.put(30600, new Color(33, 73, 109))
-      skyColorsTemp.put(30780, new Color(30, 65, 97))
-      skyColorsTemp.put(30960, new Color(26, 57, 85))
-      skyColorsTemp.put(31140, new Color(22, 48, 72))
-      skyColorsTemp.put(31320, new Color(18, 40, 60))
-      skyColorsTemp.put(31500, new Color(15, 32, 48))
-      skyColorsTemp.put(31680, new Color(11, 24, 36))
-      skyColorsTemp.put(31860, new Color(7, 16, 24))
-      skyColorsTemp.put(32040, new Color(3, 8, 12))
-      skyColorsTemp.put(32220, new Color(0, 0, 0))
-
-      skyColorsTemp.put(72000, new Color(3, 8, 12))
-      skyColorsTemp.put(72180, new Color(7, 16, 24))
-      skyColorsTemp.put(72360, new Color(11, 24, 36))
-      skyColorsTemp.put(72540, new Color(15, 32, 48))
-      skyColorsTemp.put(72720, new Color(18, 40, 60))
-      skyColorsTemp.put(72900, new Color(22, 48, 72))
-      skyColorsTemp.put(73080, new Color(26, 57, 85))
-      skyColorsTemp.put(73260, new Color(30, 65, 97))
-      skyColorsTemp.put(73440, new Color(33, 73, 109))
-      skyColorsTemp.put(73620, new Color(37, 81, 121))
-      skyColorsTemp.put(73800, new Color(41, 89, 133))
-      skyColorsTemp.put(73980, new Color(45, 97, 145))
-      skyColorsTemp.put(74160, new Color(48, 105, 157))
-      skyColorsTemp.put(74340, new Color(52, 114, 170))
-      skyColorsTemp.put(74520, new Color(56, 122, 182))
-      skyColorsTemp.put(74700, new Color(60, 130, 194))
-      skyColorsTemp.put(74880, new Color(63, 138, 206))
-      skyColorsTemp.put(75060, new Color(67, 146, 218))
-      skyColorsTemp.put(75240, new Color(71, 154, 230))
-      skyColorsTemp.put(75420, new Color(75, 163, 243))
-
-      SKYCOLORS = skyColorsTemp.asScala.toMap
 
 
       val skyLightsTemp = new jul.HashMap[Int, Int](30)
